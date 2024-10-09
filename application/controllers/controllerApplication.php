d5bead7392a44163dec57f2d59c2697b1ecd98b7
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-10-09 17:20:29 +0200

    prevent showing application when using the framework as submodule

--- application/controllers/controllerApplication.php
+++ application/controllers/controllerApplication.php
@@ -5,6 +5,12 @@ class ControllerApplication extends ControllerBase
 
     public function executeAction($action, $level, $parameters)
     {
+        // Prevent showing ths application when the framework is used as submodule
+        $chunks = array_filter(explode("/", WEB_PAGE_FOLDER));
+        if (count($chunks) > 1) {
+            http_response_code(404);
+            die();
+        }
         return $this->$action($parameters);
     }
 
